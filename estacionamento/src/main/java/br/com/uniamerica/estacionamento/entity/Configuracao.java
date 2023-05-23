0bfeda8ad39b92dff64240f8d30e313085b11a13
MatheuZAguiar <matheuspaguiar099@gmail.com>
2023-05-23 20:01:14 -0300

    Validations fixes

--- estacionamento/src/main/java/br/com/uniamerica/estacionamento/entity/Configuracao.java
+++ estacionamento/src/main/java/br/com/uniamerica/estacionamento/entity/Configuracao.java
@@ -6,41 +6,61 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalTime;
 
 @Entity
 @Table(name = "configuracoes", schema = "public")
 public class Configuracao extends AbstractEntity{
+
     @Getter @Setter
+    @NotNull(message = "O valor da hora não pode ser nulo")
+    @DecimalMin(value = "0.0", inclusive = false, message = "O valor da hora deve ser maior que 0")
     @Column(name = "valor_hora", nullable = false)
     private BigDecimal valorHora;
+
     @Getter @Setter
+    @NotNull(message = "O valor do minuto de multa não pode ser nulo")
+    @DecimalMin(value = "0.0", inclusive = false, message = "O valor do minuto de multa deve ser maior que 0")
     @Column(name = "valor_minuto_multa", nullable = false)
     private BigDecimal valorMinutoMulta;
+
     @Getter @Setter
+    @NotNull(message = "O horário de início de expediente não pode ser nulo")
     @Column(name = "inicio_expediente", nullable = false)
     private LocalTime inicioExpediente;
+
     @Getter @Setter
+    @NotNull(message = "O horário de fim de expediente não pode ser nulo")
     @Column(name = "fim_expediente", nullable = false)
     private LocalTime fimExpediente;
+
     @Getter @Setter
+    @NotNull(message = "O tempo para desconto não pode ser nulo")
     @Column(name = "tempo_para_desconto", nullable = false)
     private LocalTime tempoParaDesconto;
+
     @Getter @Setter
-    @Column(name = "tempo_desconto",nullable = false)
+    @NotNull(message = "O tempo de desconto não pode ser nulo")
+    @Column(name = "tempo_desconto", nullable = false)
     private LocalTime tempoDeDesconto;
+
     @Getter @Setter
-    @Column(name = "gerar_desconto")
     private boolean gerarDesconto;
+
     @Getter @Setter
+    @Min(value = 0, message = "O número de vagas para moto não pode ser menor que 0")
     @Column(name = "vagas_moto", nullable = false)
     private int vagasMoto;
+
     @Getter @Setter
+    @Min(value = 0, message = "O número de vagas para carro não pode ser menor que 0")
     @Column(name = "vagas_carro", nullable = false)
     private int vagasCarro;
+
     @Getter @Setter
+    @Min(value = 0, message = "O número de vagas para van não pode ser menor que 0")
     @Column(name = "vagas_van", nullable = false)
     private int vagasVan;
-
 }
--- estacionamento/src/main/java/br/com/uniamerica/estacionamento/entity/Movimentacao.java
+++ estacionamento/src/main/java/br/com/uniamerica/estacionamento/entity/Movimentacao.java
@@ -1,14 +1,13 @@
 package br.com.uniamerica.estacionamento.entity;
 
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.math.BigDecimal;
 import java.sql.Time;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "movimentacoes", schema = "public")
@@ -16,39 +15,55 @@ public class Movimentacao extends AbstractEntity{
     @Getter @Setter
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "condutores", nullable = false)
+    @NotNull(message = "O condutor é obrigatório")
     private Condutor condutor;
+
     @Getter @Setter
     @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "veiculos", nullable = false)
+    @NotNull(message = "O veículo é obrigatório")
     private Veiculo veiculo;
+
     @Getter @Setter
-    @Column(name = "data_entrada",nullable = false)
+    @Column(name = "data_entrada", nullable = false)
+    @NotNull(message = "A data de entrada é obrigatória")
     private LocalDateTime dataEntrada;
+
     @Getter @Setter
     @Column(name = "data_saida")
     private LocalDateTime dataSaida;
+
     @Getter @Setter
     @Column(name = "tempo")
     private LocalTime tempo;
+
     @Getter @Setter
     @Column(name = "tempo_desconto")
     private Time tempoDesconto;
+
     @Getter @Setter
     @Column(name = "tempo_multa")
     private LocalTime tempoMulta;
+
     @Getter @Setter
     @Column(name = "valor_desconto")
     private BigDecimal valorDesconto;
+
     @Getter @Setter
-    @Column(name = "valor_hora",nullable = false)
+    @Column(name = "valor_hora", nullable = false)
+    @NotNull(message = "O valor da hora é obrigatório")
     private BigDecimal valorHora;
+
     @Getter @Setter
     @Column(name = "valor_total")
     private BigDecimal valorTotal;
+
     @Getter @Setter
     @Column(name = "valor_multa")
     private BigDecimal valorMulta;
+
     @Getter @Setter
     @Column(name = "valor_hora_multa")
     private BigDecimal valorHoraMulta;
+
 }
